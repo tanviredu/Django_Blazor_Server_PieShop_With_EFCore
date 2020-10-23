32a878de9da0bb93c47b5708263dfe885b5b8c04
tanviredu <ornobtanvir.git@gmail.com>
2020-10-24 00:46:58 +0600

    added

--- 2_Blazor_EfCore_PieShop/PieShopFullProject/Pages/PieShopApplication/EmployeeOverview.cs
+++ 2_Blazor_EfCore_PieShop/PieShopFullProject/Pages/PieShopApplication/EmployeeOverview.cs
@@ -9,11 +9,25 @@ namespace PieShopFullProject.Pages.PieShopApplication
     public partial class EmployeeOverview
     {
         public IEnumerable<Employee> Employees { get; set; }
+        public Employee Employee { get; set; }
 
         protected override void OnInitialized()
         {
             var _db = new ApplicationDbContext();
             Employees = _db.Employees.Select(x => x).ToList();
+        }
+
+        void Delete(int id) {
+            using (var _db = new ApplicationDbContext()) {
+                Employee = _db.Employees.FirstOrDefault(e => e.EmployeeId == id);
+                _db.Employees.Remove(Employee);
+                _db.SaveChanges();
+
+                // regresh the page
+                OnInitialized();
+
+
+            }   
         }
     }
 
