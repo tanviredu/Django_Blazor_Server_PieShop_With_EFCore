6b7687c157208c690c27777b4c04235d96525a09
tanviredu <ornobtanvir.git@gmail.com>
2020-10-23 23:54:53 +0600

    added

--- 2_Blazor_EfCore_PieShop/PieShopFullProject/Models/Employee.cs
+++ 2_Blazor_EfCore_PieShop/PieShopFullProject/Models/Employee.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace PieShopFullProject.Models
 {
     public class Employee
     {
+        
         public int EmployeeId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        
         public DateTime BirthDate { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string Street { get; set; }
         public string zip { get; set; }
---
+++ 2_Blazor_EfCore_PieShop/PieShopFullProject/Pages/PieShopApplication/AddEmployee.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using PieShopFullProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShopFullProject.Pages.PieShopApplication
+{
+    public partial class AddEmployee
+    {
+        [Inject]
+        public NavigationManager navigation { get; set; }
+        public Employee Employee = new Employee();
+        public string CountryId = string.Empty;
+        public string JobCategoryId = string.Empty;
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+        protected override void OnInitialized()
+        {
+            var _db = new ApplicationDbContext();
+            Countries = _db.Countries.Select(x => x).ToList();
+            JobCategories = _db.JobCategories.Select(x => x).ToList();
+        
+
+        }
+
+        public void Add() {
+            Employee.CountryId = int.Parse(CountryId);
+            Employee.JobCategoryId = int.Parse(JobCategoryId);
+            using (var _context = new ApplicationDbContext()) {
+                _context.Employees.Add(Employee);
+                int row_affected = _context.SaveChanges();
+                if (row_affected > 0) {
+                    var Employee = new Employee();
+                    navigation.NavigateTo("/employeeoverview");
+                }
+                
+            }
+        
+        
+        }
+
+    }
+
+    
+
+}
