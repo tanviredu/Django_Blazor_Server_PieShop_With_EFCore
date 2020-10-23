452bf35ab318b3f92a1e3477fbade58b7684a9b4
tanviredu <ornobtanvir.git@gmail.com>
2020-10-23 22:36:29 +0600

    added

---
+++ 2_Blazor_EfCore_PieShop/PieShopFullProject/Pages/PieShopApplication/EmployeeDetails.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using PieShopFullProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShopFullProject.Pages.PieShopApplication
+{
+    public partial class EmployeeDetails
+    {
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+        public IEnumerable<Employee> Employees { get; set; }
+        protected override void OnInitialized()
+        {
+            var _db = new ApplicationDbContext();
+            Employees = _db.Employees.Select(x => x).ToList();
+            Employee = Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+        }
+    }
+}
---
+++ 2_Blazor_EfCore_PieShop/PieShopFullProject/Pages/PieShopApplication/EmployeeOverview.cs
@@ -0,0 +1,21 @@
+﻿using PieShopFullProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShopFullProject.Pages.PieShopApplication
+{
+    public partial class EmployeeOverview
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var _db = new ApplicationDbContext();
+            Employees = _db.Employees.Select(x => x).ToList();
+        }
+    }
+
+    
+}
